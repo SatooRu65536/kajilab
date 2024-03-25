a0f7c4cbe1b1a1f65ea776f70a7d96d81e4083a6
satooru65536 <ryuto4619@gmail.com>
2024-03-26 00:15:18 +0900

    第48週目 ~0312 fin

--- 0306-0312/main.py
+++ 0306-0312/main.py
@@ -1,42 +1,20 @@
-import asyncio,logging
-import bleak
-from bleak import BleakClient
+import asyncio
+from bleak import BleakScanner
+from bleak.uuids import normalize_uuid_str
 
-beacon_uuid = "FDA50693-A4E2-4fb1-AFCF-C6EB07647825"
 
-def UUID_from_uuid(uuid):
-    """
-    To reconstruct the full 128-bit UUID from the shortened version, 
-    insert the 16- or 32-bit short value (indicated by xxxxxxxx, 
-    including leading zeros) into the Bluetooth Base UUID:
-    xxxxxxxx-0000-1000-8000-00805F9B34FB
-    """
-    return f"{uuid:08x}-0000-1000-8000-00805f9b34fb"
+def on_advertisement_received(device, advertisement_data):
+    value = advertisement_data.service_data.values()
+    for v in value:
+        print(v)
 
-# 定義済み　16bits UUID の幾つか
-SYSTEM_ID_UUID = UUID_from_uuid(0x2A23)  # System ID
-MODEL_NBR_UUID = UUID_from_uuid(0x2A24)  # Model Number String "00002a24-0000-1000-8000-00805f9b34fb"　
-SER_NBR_UUID   = UUID_from_uuid(0x2A25)  # Serial Number String
-FIRMW_REV_UUID = UUID_from_uuid(0x2A26)  # Firmware Revision String
-HW_REV_UUID    = UUID_from_uuid(0x2A27)  # Hardware Revision String
-SW_REV_UUID    = UUID_from_uuid(0x2A28)  # Software Revision String
-MANU_NAME_UUID = UUID_from_uuid(0x2A29)  # Manufacturer Name String
-REG_CERT_UUID  = UUID_from_uuid(0x2A2A)  # IEEE 11073-20601 Regulatory Certification Data List
-CUR_TM_UUID    = UUID_from_uuid(0x2A2B)  # Current Time
 
-async def main(address):
-    async with BleakClient(address) as client:
-        if not client.is_connected:
-            await client.connect()
-        model_number = await client.read_gatt_char(MODEL_NBR_UUID) 
-        print("Model Number: {0}".format(model_number.decode('ascii'))) # Model Numberは文字列型
-        try:
-            serial_number = await client.read_gatt_char(SER_NBR_UUID) 
-            print("Serial Number: {0}".format(serial_number.decode('ascii'))) 
-        except bleak.BleakError as m:
-            logging.warning(m)
-        manufacture = await client.read_gatt_char(MANU_NAME_UUID) 
-        print("Manufacture: {0}".format(manufacture.decode('utf-8'))) #
+async def start_scan():
+    ble_scanner = BleakScanner(on_advertisement_received)
 
-asyncio.run(main(beacon_uuid))
-# await main(ipad_uuid)
+    await ble_scanner.start()
+    while True:
+        await asyncio.sleep(1.0)
+
+
+asyncio.run(start_scan())
